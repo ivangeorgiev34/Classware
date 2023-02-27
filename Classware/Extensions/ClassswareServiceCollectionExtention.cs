79e9993e6cea45302d96b5a7fe27428a8582cbff
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2023-02-27 19:41:00 +0200

    Add service extensions

--- Classware/Extensions/ClassswareServiceCollectionExtention.cs
+++ Classware/Extensions/ClassswareServiceCollectionExtention.cs
@@ -2,6 +2,11 @@
 {
 	public static class ClassswareServiceCollectionExtention
 	{
+		/// <summary>
+		/// Adds all the services to the IoC of the application
+		/// </summary>
+		/// <param name="services"></param>
+		/// <returns></returns>
 		public static IServiceCollection AddApplicationServices(this IServiceCollection services)
 		{
 
