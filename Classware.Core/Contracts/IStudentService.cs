cef9960d802cf44a585550cbbeb2658a0e18017d
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2023-03-08 20:33:09 +0200

    Add student service

---
+++ Classware.Core/Contracts/IStudentService.cs
@@ -0,0 +1,24 @@
+﻿using Classware.Infrastructure.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classware.Core.Contracts
+{
+    public interface IStudentService
+    {
+		Task AddStudentAsync(Student student);
+
+		Task<IEnumerable<Student>> GetAllStudentsAsync();
+
+		Task DeleteStudentByIdAsync(int id);
+
+		Task<Student> GetStudentByIdAsync(int id);
+
+		Task AssignSubjectsAsync(ICollection<int> subjectIds,int studentId);
+
+		bool AddGradeToStudentAsync(Student student,string subjectName,int grade);
+	}
+}
---
+++ Classware.Core/Services/StudentService.cs
@@ -0,0 +1,116 @@
+﻿using Classware.Core.Contracts;
+using Classware.Infrastructure.Common;
+using Classware.Infrastructure.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classware.Core.Services
+{
+	public class StudentService : IStudentService
+	{
+		private readonly IRepository repo;
+		private readonly ISubjectService subjectService;
+
+		public StudentService(IRepository _repo,
+			ISubjectService _subjectService)
+		{
+			repo = _repo;
+			subjectService = _subjectService;
+		}
+		public async Task AddStudentAsync(Student student)
+		{
+			await repo.AddAsync(student);
+
+			await repo.SaveChangesAsync();
+		}
+
+		public async Task AssignSubjectsAsync(ICollection<int> subjectIds, int studentId)
+		{
+			if (subjectIds == null)
+			{
+				throw new NullReferenceException("No subject ids were given");
+			}
+
+			var subjects = await subjectService.GetAllSubjectsByIdsAsync(subjectIds);
+
+			var student = await GetStudentByIdAsync(studentId);
+
+			foreach ( var subject in subjects)
+			{
+				if (student.StudentSubjects.Any(ss=>ss.SubjectId == subject.Id))
+				{
+					continue;
+				}
+
+				var studentSubjectPair = new StudentSubject()
+				{
+					SubjectId = subject.Id,
+					StudentId = studentId
+				};
+
+				student.StudentSubjects.Add(studentSubjectPair);
+			}
+
+			await repo.SaveChangesAsync();
+		}
+
+		public async Task DeleteStudentByIdAsync(int id)
+		{
+			var student = await GetStudentByIdAsync(id);
+
+			student.IsActive = false;
+
+			await repo.SaveChangesAsync();
+		}
+
+		public async Task<IEnumerable<Student>> GetAllStudentsAsync()
+		{
+			return await repo.All<Student>()
+				.Include(s => s.Class)
+				.Include(s => s.User)
+				.Include(s => s.StudentSubjects)
+				.ThenInclude(s => s.Subject)
+				.Where(s => s.IsActive)
+				.ToListAsync();
+		}
+
+		public async Task<Student> GetStudentByIdAsync(int id)
+		{
+			var student = await repo.All<Student>()
+				.Include(s => s.User)
+				.Include(s => s.Class)
+				.Include(s=>s.StudentSubjects)
+				.ThenInclude(ss=>ss.Subject)
+				.FirstOrDefaultAsync(s => s.Id == id && s.IsActive == true);
+
+			if (student == null)
+			{
+				throw new InvalidOperationException("Student with such id doesn't exist");
+			}
+			return student;
+		}
+
+		public bool AddGradeToStudentAsync(Student student,string subjectName,int grade)
+		{
+			foreach (var item in student.StudentSubjects)
+			{
+				if (item.Subject?.Name == subjectName)
+				{
+					item.Subject.Grades.Add(new Grade()
+					{
+						Type = grade,
+						Subject = item.Subject
+					});
+
+					return true;
+				}
+			}
+
+			return false;
+		}
+	}
+}
