eec2214df1d0e1d5d2b5a45c47c657da6b20cd42
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2023-03-08 20:35:01 +0200

    Add toastr messages constants

---
+++ Classware.Core/Constants/UserMessagesConstants.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classware.Core.Constants
+{
+    public static class UserMessagesConstants
+    {
+        public const string ERROR_MESSAGE = "ErrorMessage";
+
+        public const string WARNING_MESSAGE = "WarningMessage";
+
+        public const string SUCCESS_MESSAGE = "SuccessMessage";
+    }
+}
