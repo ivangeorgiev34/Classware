155b87541b6bcafb6ff9454020ecf5a8053b4d06
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2023-03-08 20:33:30 +0200

    Add subject service

---
+++ Classware.Core/Contracts/ISubjectService.cs
@@ -0,0 +1,21 @@
+﻿using Classware.Infrastructure.Models;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion.Internal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classware.Core.Contracts
+{
+	public interface ISubjectService
+	{
+		Task AddSubjectAsync(Subject subject);
+
+		Task<bool> SubjectExistsByNameAsync(string name);
+
+		Task<IEnumerable<Subject>> GetAllSubjectsAsync();
+
+		Task<ICollection<Subject>> GetAllSubjectsByIdsAsync(ICollection<int> subjectIds);
+	}
+}
---
+++ Classware.Core/Services/SubjectService.cs
@@ -0,0 +1,72 @@
+﻿using Classware.Core.Contracts;
+using Classware.Infrastructure.Common;
+using Classware.Infrastructure.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classware.Core.Services
+{
+	public class SubjectService : ISubjectService
+	{
+		private readonly IRepository repo;
+
+		public SubjectService(IRepository _repo)
+		{
+			repo = _repo;
+		}
+		public async Task AddSubjectAsync(Subject subject)
+		{
+			await repo.AddAsync(subject);
+
+			await repo.SaveChangesAsync();
+		}
+
+		public async Task<IEnumerable<Subject>> GetAllSubjectsAsync()
+		{
+			return await repo.All<Subject>()
+				.Include(s => s.StudentSubjects)
+				.ThenInclude(s => s.Student)
+				.Where(s => s.IsActive)
+				.ToListAsync();
+		}
+
+		public async Task<ICollection<Subject>> GetAllSubjectsByIdsAsync(ICollection<int> subjectIds)
+		{
+			var allSubjects = await repo.All<Subject>()
+				.Where(s => s.IsActive)
+				.ToListAsync();
+
+			var subjects = new List<Subject>();
+
+			foreach (var id in subjectIds)
+			{
+				foreach (var subject in allSubjects)
+				{
+					if (subject.Id == id)
+					{
+						subjects.Add(subject);
+
+						break;
+					}
+				}
+			}
+
+			return subjects;
+		}
+
+		public async Task<bool> SubjectExistsByNameAsync(string name)
+		{
+			if (await repo.AllReadonly<Subject>().AnyAsync(s => s.Name == name && s.IsActive == true))
+			{
+				return true;
+			}
+			return false;
+		}
+
+
+	}
+}
