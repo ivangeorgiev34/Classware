7f0ba32757d5eb79c697a8c096a4c9f15cf2154d
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2023-03-14 20:35:15 +0200

    Add compliment information action,view and view model

--- Classware/Areas/Student/Controllers/ComplimentController.cs
+++ Classware/Areas/Student/Controllers/ComplimentController.cs
@@ -2,16 +2,20 @@
 using Classware.Core.Contracts;
 using Classware.Extensions;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.Operations;
 
 namespace Classware.Areas.Student.Controllers
 {
 	public class ComplimentController : BaseController
 	{
 		private readonly IStudentService studentService;
+		private readonly IComplimentService complimentService;
 
-        public ComplimentController(IStudentService _studentService)
+		public ComplimentController(IStudentService _studentService,
+			IComplimentService _complimentService)
         {
             studentService = _studentService;
+			complimentService = _complimentService;
         }
         [HttpGet]
 		public async Task<IActionResult> All()
@@ -49,6 +53,35 @@ namespace Classware.Areas.Student.Controllers
 				throw;
 			}
 		
+		}
+
+		[HttpGet]
+		public async Task<IActionResult> ComplimentInformation(int id)
+		{
+			try
+			{
+				var compliment = await complimentService.GetComplimentByIdAsync(id);
+
+				var student = await studentService.GetStudentByUserIdAsync(User.Id());
+
+				var model = new ComplimentInformationViewModel()
+				{
+					Title = compliment.Title,
+					Description = compliment.Description,
+					FirstName = student.User.FirstName,
+					MiddleName = student.User.MiddleName,
+					LastName = student.User.LastName,
+					SubjectName = compliment.Subject.Name,
+					TeacherName = $"{compliment.Teacher.User.FirstName} {compliment.Teacher.User.MiddleName} {compliment.Teacher.User.LastName}"
+				};
+
+				return View(model);
+			}
+			catch (Exception)
+			{
+
+				throw;
+			}
 		}
 	}
 }
