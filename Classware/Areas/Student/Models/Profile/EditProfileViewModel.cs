c44e61bbd1d61ba1428935212d1812953b133351
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2023-03-15 17:38:24 +0200

    Add edit profile action and view model in student area

---
+++ Classware/Areas/Student/Models/Profile/EditProfileViewModel.cs
@@ -0,0 +1,25 @@
+﻿namespace Classware.Areas.Student.Models.Profile
+{
+	public class EditProfileViewModel
+	{
+		public string Id { get; set; } = null!;
+
+		public string? UploadedProfilePicture { get; set; }
+
+		public IFormFile? ProfilePicture { get; set; }
+
+		public string? ProfilePictureName { get; set; }
+
+		public string? FirstName { get; set; }
+
+		public string? MiddleName { get; set; }
+
+		public string? LastName { get; set; }
+
+		public string? Username { get; set; }
+
+		public int? Age { get; set; }
+
+		public string? Gender { get; set; }
+	}
+}
