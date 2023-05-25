601e51b8327f71e25e3f387f803a994122a1858f
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2023-05-25 22:58:38 +0300

    Add ChatController initial commit

---
+++ Classware/Controllers/ChatController.cs
@@ -0,0 +1,17 @@
+﻿using Classware.Models.Chat;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Classware.Controllers
+{
+    public class ChatController : BaseController
+    {
+        [AllowAnonymous]
+        [HttpGet]
+        public async Task<IActionResult> SendMessage()
+        {
+            return View();
+        }
+
+    }
+}
