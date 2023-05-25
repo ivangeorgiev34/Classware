9078cb0aef4fc9535c0bbaf5f963bd713ab13592
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2023-05-25 22:53:05 +0300

    Add chat.js initial commit

---
+++ Classware/wwwroot/js/chat.js
@@ -0,0 +1,27 @@
+﻿
+
+var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
+
+let button = document.querySelector("#sendBtn");
+button.addEventListener("click", function (event) {
+
+    event.preventDefault();
+
+    let fullName = document.querySelector(".full-name-input").value;
+    let email = document.querySelector(".email-input").value;
+    let title = document.querySelector(".title-input").value;
+    let description = document.querySelector(".description-input").value;
+
+    connection.invoke("SendMessageToAdmins", fullName, email, title, description).catch(function (error) {
+        return console.error(error.toString());
+    })
+
+});
+
+button.disabled = true;
+
+connection.start().then(function () {
+    button.disabled = false;
+}).catch(function (error) {
+    return console.error(error.toString());
+});
