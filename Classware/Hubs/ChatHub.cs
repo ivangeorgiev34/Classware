7cf9b120a372b48717a52231420d0539705af72b
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2023-05-25 22:57:10 +0300

    Add ChatHub class initial commit

---
+++ Classware/Hubs/ChatHub.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace Classware.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessageToAdmins(string fullName, string email,string title,string description)
+        {
+            await this.Clients.Group("Admin").SendAsync("RecieveMessage",fullName,email,title,description);
+        }
+
+        public async Task AddAdmin()
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, "Admin");
+        }
+    }
+}
