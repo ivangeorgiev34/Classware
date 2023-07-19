24182260fd035292c827cc47152b6e9101ae2602
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2023-07-19 18:36:45 +0300

    Add MessageService class for managing message business logic

---
+++ Classware.Core/Services/MessageService.cs
@@ -0,0 +1,59 @@
+﻿using Classware.Core.Contracts;
+using Classware.Infrastructure.Common;
+using Classware.Infrastructure.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classware.Core.Services
+{
+	public class MessageService : IMessageService
+	{
+		private readonly IRepository repo;
+		public MessageService(IRepository _repo)
+		{
+			this.repo = _repo;
+		}
+
+		public async Task<int> AddMessageAsync(string fullName, string email, string title, string description)
+		{
+			var message = new Message()
+			{
+				Email = email,
+				FullName = fullName,
+				Description = description,
+				Title = title,
+				IsAnswered = false
+			};
+
+			await repo.AddAsync(message);
+
+			await repo.SaveChangesAsync();
+
+			return message.Id;
+		}
+
+		public async Task<List<Message>> GetAllMessagesAsync()
+		{
+			return await repo.All<Message>()
+				.Where(m => m.IsAnswered == false)
+				.ToListAsync();
+		}
+
+		public async Task SetMessageToAnsweredAsync(int messageId)
+		{
+			var message = await repo.All<Message>()
+				.FirstOrDefaultAsync(m => m.Id == messageId);
+
+			if (message != null)
+			{
+				message.IsAnswered = true;
+
+				await repo.SaveChangesAsync();
+			}
+		}
+	}
+}
