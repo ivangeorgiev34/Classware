8b7c62a94334c489390c0d860339cc9a22a9909f
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2023-07-19 18:36:14 +0300

    Add IMessageService interface

---
+++ Classware.Core/Contracts/IMessageService.cs
@@ -0,0 +1,18 @@
+﻿using Classware.Infrastructure.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classware.Core.Contracts
+{
+	public interface IMessageService
+	{
+		Task<int> AddMessageAsync(string fullName, string email, string title, string description);
+
+		Task<List<Message>> GetAllMessagesAsync();
+
+		Task SetMessageToAnsweredAsync(int messageId);
+	}
+}
