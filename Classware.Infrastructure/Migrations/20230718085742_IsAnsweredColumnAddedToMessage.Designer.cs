b01c51521e2ea1fb2ac3f8be987e8d3bda679aa1
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2023-07-19 18:33:38 +0300

    Add Message entity in database

--- Classware.Infrastructure/Migrations/20230718085742_IsAnsweredColumnAddedToMessage.Designer.cs
+++ Classware.Infrastructure/Migrations/20230718085742_IsAnsweredColumnAddedToMessage.Designer.cs
@@ -4,6 +4,7 @@ using Classware.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Classware.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230718085742_IsAnsweredColumnAddedToMessage")]
+    partial class IsAnsweredColumnAddedToMessage
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Classware.Infrastructure/Migrations/20230718085742_IsAnsweredColumnAddedToMessage.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Classware.Infrastructure.Migrations
+{
+    public partial class IsAnsweredColumnAddedToMessage : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsAnswered",
+                table: "Messages",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsAnswered",
+                table: "Messages");
+        }
+    }
+}
