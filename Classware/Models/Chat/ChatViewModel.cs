eabf5b793e60e93ed71441523eb8bc4acbf79e2c
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2023-07-20 11:38:20 +0300

    Add ChatViewModel for AllMessages view

---
+++ Classware/Models/Chat/ChatViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace Classware.Models.Chat
+{
+	public class ChatViewModel
+	{
+		public int Id { get; set; }
+
+		public string FullName { get; set; } = null!;
+
+		public string Email { get; set; } = null!;
+
+		public string Title { get; set; } = null!;
+
+		public string Description { get; set; } = null!;
+	}
+}
