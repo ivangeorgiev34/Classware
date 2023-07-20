05dc49b2670492d58d79530a0879a19f61039cef
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2023-07-20 11:37:35 +0300

    Add SetMessageToAnswered method to set message to be answered by message id

--- Classware/Hubs/ChatHub.cs
+++ Classware/Hubs/ChatHub.cs
@@ -1,17 +1,32 @@
-﻿using Microsoft.AspNetCore.SignalR;
+﻿using Classware.Core.Contracts;
+using Classware.Core.Services;
+using Microsoft.AspNetCore.SignalR;
 
 namespace Classware.Hubs
 {
-    public class ChatHub : Hub
-    {
-        public async Task SendMessageToAdmins(string fullName, string email,string title,string description)
-        {
-            await this.Clients.Group("Admin").SendAsync("RecieveMessage",fullName,email,title,description);
-        }
+	public class ChatHub : Hub
+	{
+		private readonly IMessageService messageService;
+		public ChatHub(IMessageService _messageService)
+		{
+			messageService = _messageService;
+		}
 
-        public async Task AddAdmin()
-        {
-            await Groups.AddToGroupAsync(Context.ConnectionId, "Admin");
-        }
-    }
+		public async Task SendMessageToAdmins(string fullName, string email, string title, string description)
+		{
+			var messageId = await messageService.AddMessageAsync(fullName, email, title, description);
+
+			await this.Clients.Group("Admin").SendAsync("RecieveMessage", fullName, email, title, description, messageId);
+		}
+
+		public async Task AddAdmin()
+		{
+			await Groups.AddToGroupAsync(Context.ConnectionId, "Admin");
+		}
+
+		public async Task SetMessageToAnswered(int messageId)
+		{
+			await messageService.SetMessageToAnsweredAsync(messageId);	
+		}
+	}
 }
