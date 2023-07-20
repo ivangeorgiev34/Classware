335758da2b341dc6a974926ed99653b82ded441a
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2023-07-20 11:36:51 +0300

    Add all messages model to view

--- Classware/Areas/Administrator/Controllers/ChatController.cs
+++ Classware/Areas/Administrator/Controllers/ChatController.cs
@@ -1,14 +1,37 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Classware.Core.Contracts;
+using Classware.Models.Chat;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Classware.Areas.Administrator.Controllers
 {
 	public class ChatController : BaseController
 	{
-		[HttpGet]
+		private readonly IMessageService messageService;
+        public ChatController(IMessageService _messageService)
+        {
+				this.messageService = _messageService;
+        }
+
+        [HttpGet]
 		public async Task<IActionResult> AllMessages()
 		{
+			var messages = await messageService.GetAllMessagesAsync();
 
-			return View();
+			var model = new List<ChatViewModel>();
+
+			foreach (var message in messages)
+			{
+				model.Add(new ChatViewModel()
+				{
+					Id = message.Id,
+					Email = message.Email,
+					Description = message.Description,
+					FullName = message.FullName,
+					Title = message.Title
+				});
+			}
+
+			return View(model);
 
 		}
 	}
