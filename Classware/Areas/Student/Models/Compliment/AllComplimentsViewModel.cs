4c2be4e91f16a6f97a664fab322161670187ff1f
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2023-08-15 23:31:11 +0300

    Add TotalCompliments and Page properties to AllComplimentsViewModel in student area

--- Classware/Areas/Student/Models/Compliment/AllComplimentsViewModel.cs
+++ Classware/Areas/Student/Models/Compliment/AllComplimentsViewModel.cs
@@ -2,6 +2,10 @@
 {
 	public class AllComplimentsViewModel
 	{
+        public int TotalCompliments { get; set; }
+
+        public int Page { get; set; }
+
         public ICollection<ComplimentViewModel>? Compliments { get; set; }
     }
 }
