b4946656ddcec6cdee6411806eeb4500dc28bdbd
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2023-08-15 19:48:06 +0300

    Add ClassViewModel

---
+++ Classware/Areas/Teacher/Models/Class/ClassViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Classware.Areas.Teacher.Models.Class
+{
+	public class ClassViewModel
+	{
+		public string Id { get; set; } = null!;
+
+		public string Name { get; set; } = null!;
+
+		public int StudentsCount { get; set; }
+	}
+}
