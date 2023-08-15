4dacf41053b17b74fbbead023479e64db730b8ff
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2023-08-16 00:59:43 +0300

    Add Id, TotalProperties, and Page properties to ClassStudentsViewModel

--- Classware/Areas/Teacher/Models/Class/ClassStudentsViewModel.cs
+++ Classware/Areas/Teacher/Models/Class/ClassStudentsViewModel.cs
@@ -2,6 +2,11 @@
 {
 	public class ClassStudentsViewModel
 	{
+        public string Id { get; set; }
+        public int TotalProperties { get; set; }
+
+        public int Page { get; set; }
+
         public ICollection<StudentViewModel>? Students { get; set; }
     }
 }
