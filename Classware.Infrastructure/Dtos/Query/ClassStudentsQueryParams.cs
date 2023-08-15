ed92c0947d9492fed6fde91f333424cc671e8805
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2023-08-16 00:57:49 +0300

    Add ClassStudentsQueryParams dto for query string params

---
+++ Classware.Infrastructure/Dtos/Query/ClassStudentsQueryParams.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classware.Infrastructure.Dtos.Query
+{
+	public class ClassStudentsQueryParams
+	{
+		public int Page { get; set; } = 1;
+
+        public string? FirstName { get; set; }
+
+		public string? MiddleName { get; set; }
+
+		public string? LastName { get; set; }
+	}
+}
