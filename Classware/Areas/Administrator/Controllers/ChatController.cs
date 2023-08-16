4d63cec977a5f2bda749b3edac1f9de5efa38c49
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2023-08-16 19:58:24 +0300

    Add pagination functionality to AllMessages action in admin area

--- Classware/Areas/Administrator/Controllers/ChatController.cs
+++ Classware/Areas/Administrator/Controllers/ChatController.cs
@@ -1,4 +1,5 @@
-﻿using Classware.Core.Contracts;
+﻿using Classware.Areas.Administrator.Models.Chat;
+using Classware.Core.Contracts;
 using Classware.Models.Chat;
 using Microsoft.AspNetCore.Mvc;
 
@@ -7,21 +8,30 @@ namespace Classware.Areas.Administrator.Controllers
 	public class ChatController : BaseController
 	{
 		private readonly IMessageService messageService;
-        public ChatController(IMessageService _messageService)
-        {
-				this.messageService = _messageService;
-        }
+		public ChatController(IMessageService _messageService)
+		{
+			this.messageService = _messageService;
+		}
 
-        [HttpGet]
-		public async Task<IActionResult> AllMessages()
+		[HttpGet]
+		public async Task<IActionResult> AllMessages(int page = 1)
 		{
 			var messages = await messageService.GetAllMessagesAsync();
 
-			var model = new List<ChatViewModel>();
-
-			foreach (var message in messages)
+			var model = new AllMessagesViewModel()
 			{
-				model.Add(new ChatViewModel()
+				Page = page,
+				TotalMessages = messages.Count()
+			};
+
+			var paginatedMessages = messages
+				.Skip((page - 1) * 4)
+				.Take(4)
+				.ToList();
+
+			foreach (var message in paginatedMessages)
+			{
+				model.Messages.Add(new ChatViewModel()
 				{
 					Id = message.Id.ToString(),
 					Email = message.Email,
