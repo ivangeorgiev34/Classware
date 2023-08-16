870f5002337287518c245e44c11819959f0bc183
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2023-08-16 19:56:45 +0300

    Add AllMessagesViewModel

---
+++ Classware/Areas/Administrator/Models/Chat/AllMessagesViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Classware.Models.Chat;
+
+namespace Classware.Areas.Administrator.Models.Chat
+{
+	public class AllMessagesViewModel
+	{
+        public AllMessagesViewModel()
+        {
+            this.Messages = new List<ChatViewModel>();
+        }
+        public int Page { get; set; }
+        public int TotalMessages { get; set; }
+        public List<ChatViewModel> Messages { get; set; }
+    }
+}
