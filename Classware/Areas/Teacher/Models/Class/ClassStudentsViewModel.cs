aa62cf7c5659cbe2739e3aa0b5903097fac0b761
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2023-08-16 15:05:00 +0300

    Add SearchOption and SearchValue properties to ClassStudentsViewModel

--- Classware/Areas/Teacher/Models/Class/ClassStudentsViewModel.cs
+++ Classware/Areas/Teacher/Models/Class/ClassStudentsViewModel.cs
@@ -2,11 +2,16 @@
 {
 	public class ClassStudentsViewModel
 	{
-        public string Id { get; set; } = null!;
-        public int TotalProperties { get; set; }
+		public string Id { get; set; } = null!;
 
-        public int Page { get; set; }
+		public int TotalProperties { get; set; }
 
-        public ICollection<StudentViewModel>? Students { get; set; }
-    }
+		public int Page { get; set; }
+
+		public string? SearchOption { get; set; }
+
+		public string? SearchValue { get; set; }
+
+		public ICollection<StudentViewModel>? Students { get; set; }
+	}
 }
